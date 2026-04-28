4d1bb201fe0a5f31fe14a981e5a9eea53abe4a92
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-04-28 12:12:55 -0500

    Enhance Person class with hobbies management
    Added Hobbies property initialization and AddHobbies method to manage hobbies.

--- Person.cs
+++ Person.cs
@@ -8,5 +8,14 @@ public class Person{
    public Person(string firstName, string lastName){
       FirstName = firstName;
       LastName = lastName;
+
+      Hobbies = new string[0];
+   }
+
+   public void AddHobbies(params string[] hobbies){
+      string[] newHobbies = new string[Hobbies.Length + hobbies.Length];
+      Hobbies.CopyTo(newHobbies, 0);
+      hobbies.CopyTo(newHobbies, Hobbies.Length);
+      Hobbies = newHobbies;
    }
 }
